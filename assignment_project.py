ccd1b151dede1c212fbf63ccfe864ead44d87d68
Raymond <ouyangmingraymond@gmail.com>
2021-03-08 17:14:38 -0800

    Cleaned up code a little

--- assignment_project.py
+++ assignment_project.py
@@ -23,25 +23,18 @@ class TheWalkingDead(gym.Env):
 
     def __init__(self, env_config):
         # Static Parameters
-        self.size = 11
-        self.reward_density = .1
-        self.penalty_density = .02
-        self.obs_size = 7
-        self.max_episode_steps = 100
+        self.size = 10
+        self.obs_size = 9   # This is the size of the observation grid
         self.log_frequency = 10
         self.action_dict = {
             0: 'move 1',  # Move one block forward
             1: 'turn 1',  # Turn 90 degrees to the right
             2: 'turn -1',  # Turn 90 degrees to the left
-            ##            3: 'attack 1'  # Destroy block (Let's comment this command out for our sanity check to make it simpler.)
-            ##                             We will add this and more commands later once we get our sanity check working.
-            ##                             Right now, our sanity check will involve the agent simply trying to run away from the zombies
-
         }
         # Rllib Parameters
-        self.num_diff_creatures = 2
+        self.num_items_in_observation_array = 3
         self.action_space = Discrete(len(self.action_dict))
-        self.observation_space = Box(0, 1, shape=(self.num_diff_creatures * self.obs_size * self.obs_size,), dtype=np.float32)
+        self.observation_space = Box(0, 1, shape=(self.num_items_in_observation_array * self.obs_size * self.obs_size,), dtype=np.float32)
 
         # Malmo Parameters
         self.agent_host = MalmoPython.AgentHost()
@@ -53,12 +46,11 @@ class TheWalkingDead(gym.Env):
             exit(1)
 
         # TheWalkingDead Parameters
-        self.is_begin = True
         self.facing_zombie = False
         self.facing_creeper = False
         self.facing_wall = False       
-        self.num_zombies = 1
-        self.num_creepers = 1
+        self.num_zombies = 2
+        self.num_creepers = 2
         self.damage_taken_so_far = 0
         self.new_damage_taken = 0
         self.obs = None
@@ -67,7 +59,7 @@ class TheWalkingDead(gym.Env):
         self.episode_return = 0
         self.returns = []
         self.steps = []
-        self.max_episode_steps = 100
+        self.max_episode_steps = 200
 
     def reset(self):
         """
@@ -110,9 +102,8 @@ class TheWalkingDead(gym.Env):
             info: <dict> dictionary of extra information
         """
         # get night vision
-        if self.is_begin:
+        if self.episode_step == 1:
             self.agent_host.sendCommand('chat /effect @p night_vision 999 99')
-            self.is_begin = False
             
         # Get Action
         if action != 'move 1' or (not self.facing_creeper and not self.facing_zombie and not self.facing_wall):
@@ -140,38 +131,34 @@ class TheWalkingDead(gym.Env):
 
     def get_mission_xml(self):
         # Draw walls around the arena
-        # Draw west wall
-        west_wall_xml = "<DrawCuboid x1='{}' x2='{}' y1='2' y2='6' z1='{}' z2='{}' type='obsidian'/>".format(
-                            -self.size - 1, -self.size - 1, 2*self.size + 1, -self.size - 1)
-        east_wall_xml = "<DrawCuboid x1='{}' x2='{}' y1='2' y2='6' z1='{}' z2='{}' type='obsidian'/>".format(
-                            self.size + 1, self.size + 1, 2*self.size + 1, -self.size - 1)
-        north_wall_xml = "<DrawCuboid x1='{}' x2='{}' y1='2' y2='6' z1='{}' z2='{}' type='obsidian'/>".format(
-                            -self.size - 1, self.size + 1, 2*self.size + 1, 2*self.size + 1)
-        south_wall_xml = "<DrawCuboid x1='{}' x2='{}' y1='2' y2='5' z1='{}' z2='{}' type='obsidian'/>".format(
-                            -self.size - 1, self.size + 1, -self.size - 1, -self.size - 1)
+        west_wall_xml = f"<DrawCuboid x1='{-self.size - 1}' x2='{-self.size - 1}' y1='2' y2='5' z1='{2*self.size + 1}' z2='{-self.size - 1}' type='end_portal_frame'/>"
+        east_wall_xml = f"<DrawCuboid x1='{self.size + 1}' x2='{self.size + 1}' y1='2' y2='5' z1='{2*self.size + 1}' z2='{-self.size - 1}' type='end_portal_frame'/>"
+        north_wall_xml = f"<DrawCuboid x1='{-self.size - 1}' x2='{self.size + 1}' y1='2' y2='5' z1='{2*self.size + 1}' z2='{2*self.size + 1}' type='end_portal_frame'/>"
+        south_wall_xml = f"<DrawCuboid x1='{-self.size - 1}' x2='{self.size + 1}' y1='2' y2='5' z1='{-self.size - 1}' z2='{-self.size - 1}' type='end_portal_frame'/>"
         walls_xml = west_wall_xml + east_wall_xml + north_wall_xml + south_wall_xml
+
         finishline = ''
-        z = 2*self.size - 1
-        for x in range(-self.size, self.size):
+        z = 2*self.size
+        for x in range(-self.size, self.size+1):
             finishline += f'<DrawBlock x=\'{x}\' y=\'1\' z = \'{z}\' type=\'diamond_block\' />'
             finishline += '\n' 
 
-        def _creature_xml_maker(entity_name: str, num_entities: int) -> str:
+        def mob_drawer(entity_name: str, num_entities: int) -> str:
             creature_xml = []
-            creature_locations = set()
+            creature_locations = set([(-1,-1),(0,-1),(1,-1),(-1,0),(0,0),(1,0),(-1,1),(0,1),(1,1)])
             for i in range(num_entities):
-                x = random.randint(-self.size + 1, self.size)
-                z = random.randint(-self.size + 1, self.size)
+                x = random.randint(-self.size + 2, self.size-1)
+                z = random.randint(-self.size + 2, self.size-1)
                 while (x, z) in creature_locations:
-                    x = random.randint(-self.size + 1, self.size)
-                    z = random.randint(-self.size + 1, self.size)
+                    x = random.randint(-self.size + 2, self.size-1)
+                    z = random.randint(-self.size + 2, self.size-1)
                 creature_locations.add((x, z))
                 creature_xml.append("<DrawEntity x='" + str(x) + "' y='2' z='" + str(z) + f"' type='{entity_name}' />")
             creature_xml = ''.join(creature_xml)
             return creature_xml
 
-        zombies_xml = _creature_xml_maker('Zombie', self.num_zombies)
-        creepers_xml = _creature_xml_maker('Creeper', self.num_creepers)
+        zombies_xml = mob_drawer('Zombie', self.num_zombies)
+        creepers_xml = mob_drawer('Creeper', self.num_creepers)
 
         return '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                 <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -192,41 +179,40 @@ class TheWalkingDead(gym.Env):
                         <ServerHandlers>
                             <FlatWorldGenerator generatorString="3;7,2;1;"/>
                             <DrawingDecorator>''' + \
-                               "<DrawCuboid x1='{}' x2='{}' y1='2' y2='7' z1='{}' z2='{}' type='air'/>".format(-100, 100, -100, 100) + \
-                               "<DrawCuboid x1='{}' x2='{}' y1='1' y2='1' z1='{}' z2='{}' type='obsidian'/>".format(-200, 200,-200, 200) + \
-                               "<DrawCuboid x1='{}' x2='{}' y1='1' y2='1' z1='{}' z2='{}' type='obsidian'/>".format(-self.size, self.size,-self.size, self.size) + \
-                               zombies_xml + \
-                               creepers_xml + \
-                               walls_xml + \
-                               finishline + \
-                               '''<DrawBlock x='0'  y='2' z='0' type='air' />
-                               </DrawingDecorator>
-                               <ServerQuitWhenAnyAgentFinishes/>
-                           </ServerHandlers>                           
-                        </ServerSection>
-                        
-                        <AgentSection mode="Survival">
-                           <Name>CS175TheWalkingDead</Name>
-                           <AgentStart>''' + \
-                               f'''<Placement x="0" y="2" z="0" pitch="45" yaw="0"/>
-                           </AgentStart>
-                           <AgentHandlers>
-                           <ObservationFromNearbyEntities>
-                                <Range name="Zombie" xrange="{self.obs_size // 2}" yrange="1" zrange="{self.obs_size // 2}"/>
-                                <Range name="Creeper" xrange="{self.obs_size // 2}" yrange="1" zrange="{self.obs_size // 2}"/>
+                                "<DrawCuboid x1='{}' x2='{}' y1='2' y2='2' z1='{}' z2='{}' type='air'/>".format(-300, 300, -300, 300) + \
+                                "<DrawCuboid x1='{}' x2='{}' y1='1' y2='1' z1='{}' z2='{}' type='obsidian'/>".format(-self.size-100, self.size+100, -self.size-100, self.size+100) + \
+                                walls_xml + \
+                                zombies_xml + \
+                                creepers_xml + \
+                                finishline + \
+                                '''<DrawBlock x='0'  y='2' z='0' type='air' />
+                            </DrawingDecorator>
+                            <ServerQuitWhenAnyAgentFinishes/>
+                        </ServerHandlers>
+                    </ServerSection>
+
+                    <AgentSection mode="Survival">
+                        <Name>CS175TheWalkingDead</Name>
+                        <AgentStart>
+                            <Placement x="0.5" y="2" z="0.5" pitch="45" yaw="0"/>
+                        </AgentStart>
+                        <AgentHandlers>''' + \
+                            f'''<ObservationFromNearbyEntities>
+                                <Range name="Zombie" xrange="{self.obs_size//2}" yrange="1" zrange="{self.obs_size//2}"/>
+                                <Range name="Creeper" xrange="{self.obs_size//2}" yrange="1" zrange="{self.obs_size//2}"/>
                             </ObservationFromNearbyEntities>
                             <ObservationFromFullStats/>
                             <ObservationFromRay/>
+                            <ObservationFromGrid>
+                                <Grid name="floorAll">
+                                    <min x="-'''+str(int(self.obs_size/2))+'''" y="2" z="-'''+str(int(self.obs_size/2))+'''"/>
+                                    <max x="'''+str(int(self.obs_size/2))+'''" y="2" z="'''+str(int(self.obs_size/2))+'''"/>
+                                </Grid>
+                            </ObservationFromGrid>
                             <RewardForTimeTaken initialReward="10" delta="+2" density="PER_TICK" />
-                            <RewardForTouchingBlockType>
-                                <Block type="diamond_block" reward="100" />                                    
-                            </RewardForTouchingBlockType>
                             <DiscreteMovementCommands/>
                             <ChatCommands/>
-                            <AgentQuitFromReachingCommandQuota total="''' + str(self.max_episode_steps) + '''" />
-                            <AgentQuitFromTouchingBlockType>
-                                <Block type="diamond_block"/>
-                            </AgentQuitFromTouchingBlockType>
+                            <AgentQuitFromReachingCommandQuota total="'''+str(self.max_episode_steps)+'''" />
                         </AgentHandlers>
                     </AgentSection>
                 </Mission>'''
@@ -269,7 +255,7 @@ class TheWalkingDead(gym.Env):
 
     def get_observation(self, world_state):
         """
-        Use the agent observation API to get a flattened 1 x 5 x 5 grid around the agent.
+        Use the agent observation API to get a flattened 1 x 5 x 5 grid around the agent. 
         The agent is in the center square facing up.
 
         Args
@@ -278,9 +264,9 @@ class TheWalkingDead(gym.Env):
         Returns
             observation: <np.array> the state observation
         """
-
-        obs = np.zeros((2 * self.obs_size * self.obs_size,))
-
+        
+        obs = np.zeros((self.num_items_in_observation_array * self.obs_size * self.obs_size, ))
+        
         while world_state.is_mission_running:
             time.sleep(0.1)
             world_state = self.agent_host.getWorldState()
@@ -292,17 +278,22 @@ class TheWalkingDead(gym.Env):
                 msg = world_state.observations[-1].text
                 observations = json.loads(msg)
                 i = 0
-                while 'DamageTaken' not in observations or 'Zombie' not in observations or 'Yaw' not in observations or 'LineOfSight' not in observations:
+                while 'DamageTaken' not in observations or \
+                'Zombie' not in observations or \
+                'Creeper' not in observations or \
+                'Yaw' not in observations or \
+                'LineOfSight' not in observations:
                     i += 1
-                    print("STUCK HERE", observations)
-                    time.sleep(0.1)
-                    msg = world_state.observations[-1].text
-                    observations = json.loads(msg)
                     if i == 5:
                         observations = self.prev_observation
                         break
-                self.prev_observation = observations
+                    time.sleep(0.1)
+                    msg = world_state.observations[-1].text
+                    observations = json.loads(msg)
+                    print(observations)
+                    print(222222222222)
                 
+                self.prev_observation = observations
                 # Record any new damage that is taken for negative reward later
                 damage_taken = observations['DamageTaken']
                 self.new_damage_taken = damage_taken - self.damage_taken_so_far
@@ -312,31 +303,32 @@ class TheWalkingDead(gym.Env):
                 agent_location = None
                 for entity in observations['Zombie']:
                     if entity['name'] == 'CS175TheWalkingDead':
-                        agent_location = (entity['x'] + self.obs_size // 2, entity['z'] + self.obs_size // 2)
-                        break
+                        agent_location = (entity['x']+self.obs_size//2, entity['z']+self.obs_size//2)
+                        break                 
 
-                # Put zombies into observation array
-                zombie_locations = list((agent_location[0] - entity['x'], agent_location[1] - entity['z']) for entity in observations['Zombie'] if entity['name'] == 'Zombie')
+                # Get zombie locations  
+                zombie_locations = list((agent_location[0]-entity['x'], agent_location[1]-entity['z']) for entity in observations['Zombie'] if entity['name'] == 'Zombie')                              
                 for i in range(self.obs_size * self.obs_size):
-                    obs[i] = False
+                    obs[i] = False  
+                    
+                for x,z in zombie_locations:
+                    obs[math.floor(z) + math.floor(x) * self.obs_size] = True 
 
-                for x, z in zombie_locations:
-                    obs[math.floor(z) + math.floor(x) * self.obs_size] = True
+                # Get creeper locations  
+                creeper_locations = list((agent_location[0]-entity['x'], agent_location[1]-entity['z']) for entity in observations['Creeper'] if entity['name'] == 'Creeper')                              
+                for i in range(self.obs_size ** 2, 2 * self.obs_size ** 2):
+                    obs[i] = False  
+                    
+                for x,z in creeper_locations:
+                    obs[self.obs_size ** 2 + math.floor(z) + math.floor(x) * self.obs_size] = True 
 
-                # Put creepers into observation array
-                creeper_locations = list((agent_location[0] - entity['x'], agent_location[1] - entity['z']) for entity in observations['Creeper'] if entity['name'] == 'Creeper')
-                for i in range(self.obs_size * self.obs_size, 2 * self.obs_size * self.obs_size):
-                    obs[i] = False
-
-                for x, z in creeper_locations:
-                    obs[self.obs_size * self.obs_size + math.floor(z) + math.floor(x) * self.obs_size] = True
-
-                #                 grid = observations['floorAll']
-                #                 for i, x in enumerate(grid):
-                #                     obs[i] = x == 'diamond_ore' or x == 'lava'
+                # Get wall locations
+                grid = observations['floorAll']
+                for i, x in enumerate(grid):
+                    obs[2 * (self.obs_size ** 2) + i] = x == 'end_portal_frame'
 
                 # Rotate observation with orientation of agent
-                obs = obs.reshape((2, self.obs_size, self.obs_size))
+                obs = obs.reshape((self.num_items_in_observation_array, self.obs_size, self.obs_size))
                 yaw = observations['Yaw']
                 if yaw >= 225 and yaw < 315:
                     obs = np.rot90(obs, k=1, axes=(1, 2))
@@ -349,10 +341,9 @@ class TheWalkingDead(gym.Env):
                 # Check if there is a zombie in front of agent
                 self.facing_zombie = observations['LineOfSight']['type'] == 'Zombie'
                 self.facing_creeper = observations['LineOfSight']['type'] == 'Creeper'
-                self.facing_wall = observations['LineOfSight']['type'] == 'obsidian'
-
+                self.facing_wall = observations['floorAll'][int((len(grid) ** 0.5) * ((len(grid) ** 0.5)//2-1) + (len(grid) ** 0.5)//2)] == 'end_portal_frame'
                 break
-
+        
         return obs
 
 
